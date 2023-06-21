1519f9f5ffbadfeb2da40ac0ad952b8296197f0e
iamjoee <joeadvocates123@gmail.com>
2023-06-21 11:58:10 -0700

    airbnb update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" """
+"""
+Module for testing the BaseModel class
+"""
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,38 +11,53 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """
+    Class to test the BaseModel class
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initialize the test class
+        """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """
+        Set up the test environment
+        """
         pass
 
     def tearDown(self):
+        """
+        Clean up after the test
+        """
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
     def test_default(self):
-        """ """
+        """
+        Test the default behavior of BaseModel instantiation
+        """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """
+        Test BaseModel instantiation with keyword arguments
+        """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """
+        Test BaseModel instantiation with non-string key in kwargs
+        """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -48,7 +65,9 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Testing save """
+        """
+        Test the save method of BaseModel
+        """
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -57,43 +76,61 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """
+        Test the __str__ method of BaseModel
+        """
         i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(
+            self.name, i.id, i.__dict__))
 
     def test_todict(self):
-        """ """
+        """
+        Test the to_dict method of BaseModel
+        """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """
+        Test BaseModel instantiation with None as key in kwargs
+        """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """
+        Test BaseModel instantiation with a single keyword argument
+        """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """
+        Test the id attribute of BaseModel
+        """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """
+        Test the created_at attribute of BaseModel
+        """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """
+        Test the updated_at attribute of BaseModel
+        """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,92 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Module for testing the Review class
+"""
+import unittest
+import pep8
+import os
+from os import getenv
 from models.review import Review
+from models.base_model import BaseModel
 
 
-class test_review(test_basemodel):
-    """ """
+class TestReview(unittest.TestCase):
+    """Class to test the Review class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    @classmethod
+    def setUpClass(cls):
+        """Set up for test"""
+        cls.rev = Review()
+        cls.rev.place_id = "4321-dcba"
+        cls.rev.user_id = "123-bca"
+        cls.rev.text = "The strongest in the Galaxy"
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down after the test"""
+        del cls.rev
+
+    def tearDown(self):
+        """Tear down"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_Review(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/review.py'])
+        self.assertEqual(p.total_errors, 0, "Fix pep8")
+
+    def test_checking_for_docstring_Review(self):
+        """Test checking for docstrings"""
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_attributes_review(self):
+        """Test if Review has attributes"""
+        self.assertTrue('id' in self.rev.__dict__)
+        self.assertTrue('created_at' in self.rev.__dict__)
+        self.assertTrue('updated_at' in self.rev.__dict__)
+        self.assertTrue('place_id' in self.rev.__dict__)
+        self.assertTrue('text' in self.rev.__dict__)
+        self.assertTrue('user_id' in self.rev.__dict__)
+
+    def test_is_subclass_Review(self):
+        """Test if Review is a subclass of BaseModel"""
+        self.assertTrue(issubclass(self.rev.__class__, BaseModel), True)
+
+    def test_attribute_types_Review(self):
+        """Test attribute types for Review"""
+        self.assertEqual(type(self.rev.text), str)
+        self.assertEqual(type(self.rev.place_id), str)
+        self.assertEqual(type(self.rev.user_id), str)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == 'db', 'DB')
+    def test_save_Review(self):
+        """Test if save works"""
+        self.rev.save()
+        self.assertNotEqual(self.rev.created_at, self.rev.updated_at)
+
+    def test_to_dict_Review(self):
+        """Test if to_dict works"""
+        self.assertEqual('to_dict' in dir(self.rev), True)
 
     def test_place_id(self):
-        """ """
-        new = self.value()
+        """Test the place_id attribute of Review"""
+        new = Review()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
-        new = self.value()
+        """Test the user_id attribute of Review"""
+        new = Review()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
-        new = self.value()
+        """Test the text attribute of Review"""
+        new = Review()
         self.assertEqual(type(new.text), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
