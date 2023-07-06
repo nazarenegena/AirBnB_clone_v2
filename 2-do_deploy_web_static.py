01737f12e823f4b4e21904c960242874e4e5388e
nazarenegena <genarene96@gmail.com>
2023-07-06 09:51:44 +0000

    refactored code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,9 +10,12 @@ env.hosts = ["54.160.85.72", "35.175.132.106"]
 
 
 def do_deploy(archive_path):
-    """function distributes an archive the web server.
+    """Distributes an archive to a web server.
     Args:
-        archive_path (str): path of the archive distribution.
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
